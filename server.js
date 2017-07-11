b915926c7247e8f6317470d229d0df151d3eb23d
SupunRanatunge <supunr.15@cse.mrt.ac.lk>
2017-07-11 12:05:49 +0530

    connecting to server by socket

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+//import libraries
 const express = require('express');
+const socketIO = require('socket.io');
+
+//local libraries
+const http = require('http');
+const path = require('path');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(__dirname+ '/oosd-project-master'));
-app.get('/', (req, res) => {
-    res.send('Hello Express!');
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
 });
 
-app.listen(3000);
+
+server.listen(3045, () => {
+    console.log('Server is up on port 3045');
+});
